7779e566155893a51b7a397f4f903107da2b65ff
chiamaka28 <maduikechiamaka4@gmail.com>
2023-03-09 22:19:59 +0100

    finished the copy button

--- scripts/script.js
+++ scripts/script.js
@@ -23,19 +23,32 @@ linkBtn.addEventListener('click', function shortenLink (e) {
             return res.json();
         })
         .then((data) => {
+            console.log(data);
           let result = data.result.short_link;
           const output = `<div class="output"><p class="old-link">${linkInput.value}<p>
                                <p class="new-link">${result}</p>
-                               <div><button class="copy" id="copy" onclick="copyToClipboard()">copy</button></div>
+                               <div><button class="copy" id="copy">copy</button></div>
                            </div>`;
          linkList.insertAdjacentHTML("afterbegin", output);
           linkInput.value = "";
         })
        
      
-        
+       
     }
 })
+
+document.querySelector('body').addEventListener('click', function (e){
+    const btn = e.target;
+    if (btn.className === "copy"){
+        btn.textContent = "Copied!";
+        btn.style.backgroundColor = " hsl(257, 27%, 26%)";
+        const newLink = document.querySelector('.new-link')
+        navigator.clipboard.writeText(newLink.textContent);
+        console.log(e.target)
+    }
+})
+
 
 
 // function  copyToClipboard (){
@@ -43,46 +56,41 @@ linkBtn.addEventListener('click', function shortenLink (e) {
 //         btn.addEventListener('click', (e) => {
 //             console.log(e);
 //         const el = e.target;
-//         if (el === btn[i]){
+//         if (el === btn){
+//             e.preventDefault();
 //         // console.log(e.target);
 //         btn.setAttribute("style" , "background-color: hsl(257, 27%, 26%);");
 //         btn.textContent = "Copied!";
-
+    
 //         const newLink = document.querySelectorAll('.new-link').forEach((link) => {
 //         navigator.clipboard.writeText(link.textContent);
 //         console.log(link.textContent);
 //     })
+    
 //         }
            
 //         })
 //     })
 // }
 
-function copyToClipboard (){
-    let buttons = [...document.querySelectorAll(".copy")];
-    for (let index = 0; index < buttons.length; index++) {
-        console.log(buttons.length);
-      buttons[index].addEventListener("click", (e) => {
-         if (buttons[index] === e.target){
-                 console.log("yess");
-       }
-      })
-}
-    // buttons.forEach(btn => btn.addEventListener('click', (e) => {
-    //     buttons.map(item => {
-    //         if (item === e.target){
-    //             console.log("yess");
-    //         }
-    //     })
-    // }))
-}
-
-// output.forEach(el => el.addEventListener("click", (e) => {
-//     console.log(e.target)
-//     if (e.target.className === "copy"){
-//         console.log(e.target);
-//     }
-// }))
+// function copyToClipboard (){
+//     let buttons = [...document.querySelectorAll(".copy")];
+//     for (let index = 0; index < buttons.length; index++) {
+//         console.log(buttons.length);
+//       buttons[index].addEventListener("click", (e) => {
+//          if (buttons[index] === e.target){
+//                  console.log("yess");
+//        }
+//       })
+// }
+//     // buttons.forEach(btn => btn.addEventListener('click', (e) => {
+//     //     buttons.map(item => {
+//     //         if (item === e.target){
+//     //             console.log("yess");
+//     //         }
+//     //     })
+//     // }))
+// }
 
 
 
@@ -90,23 +98,10 @@ function copyToClipboard (){
 
 
 
-// let copyBtn = [...document.querySelectorAll('.copy')];
-// copyBtn.forEach(el => el.addEventListener("click", (e) => {
-//     copyBtn.map(item => {
-//         if (item === e.target){
-//             item.textContent = "Copied!"
-//         }
-//     })
-// }))
-// document.querySelector('body').addEventListener('click', function (e){
-//     if (e.target.className === "copy"){
-     
 
-//         const newLink = document.querySelector('.new-link')
-//         navigator.clipboard.writeText(newLink.textContent);
-//         console.log(newLink.textContent);
-//     }
-// })
+
+
+
 
 
 
